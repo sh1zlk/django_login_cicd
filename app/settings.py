a53ff487cf2b2611d93fdbb85527e121ff14a100
Sh1zik <89149613+sh1zlk@users.noreply.github.com>
2023-09-24 22:32:59 +0300

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -1,9 +1,10 @@
 import os
 
 IS_PRODUCTION = os.environ.get('IS_PRODUCTION')
+ALLOWED_HOSTS = ['*', '158.180.20.10']
 
 if IS_PRODUCTION:
     from .conf.production.settings import *
 else:
     from .conf.development.settings import *
-ALLOWED_HOSTS = ['*', '158.180.20.10']
+
